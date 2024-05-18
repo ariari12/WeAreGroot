5aaf8a32d1d845adafe46e6b5d0217c939d0f30a
KAH <kahback99@gmail.com>
2024-05-18 23:29:49 +0900

    [fetch] 회원가입 폼 jps 작성

--- src/main/java/kr/co/moran/web/controller/MemeberController.java
+++ src/main/java/kr/co/moran/web/controller/MemeberController.java
@@ -9,36 +9,14 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("")
+@WebServlet("/web")
 public class MemeberController extends HttpServlet{
 	private void doProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("UTF-8");
 		resp.setContentType("text/html;charset=UTF-8");
+		String name = req.getParameter("name");
 		
-		// cmd 파라미터 값 가져오기
-//		String cmd =req.getParameter("cmd");
-//		String url="";
-//		Action action = null;
-//		if(cmd == null || cmd.equals("list")) {
-//			action = new ListAction();			
-//		}else if(cmd.equals("write")) {
-//			action = new WriteFormAction();
-//		}else if(cmd.equals("writeOk")) {
-//			action = new WriteAction();
-//		}else if(cmd.equals("detail")) {
-//			action = new DetailAction();
-//		}else if(cmd.equals("modify")) {
-//			action = new ModifyFormAction();
-//		}else if(cmd.equals("modifyOk")) {
-//			action = new ModifyAction();
-//		}else if(cmd.equals("delete")) {
-//			action = new DeleteAction();
-//		}
-//		
-//		
-//		url = action.execute(req, resp);
-//		RequestDispatcher rd = req.getRequestDispatcher(url);
-//		rd.forward(req, resp);
+
 	}
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -49,3 +27,29 @@ public class MemeberController extends HttpServlet{
 		doProcess(req,resp);
 	}
 }
+
+
+// cmd 파라미터 값 가져오기
+//String cmd =req.getParameter("cmd");
+//String url="";
+//Action action = null;
+//if(cmd == null || cmd.equals("list")) {
+//	action = new ListAction();			
+//}else if(cmd.equals("write")) {
+//	action = new WriteFormAction();
+//}else if(cmd.equals("writeOk")) {
+//	action = new WriteAction();
+//}else if(cmd.equals("detail")) {
+//	action = new DetailAction();
+//}else if(cmd.equals("modify")) {
+//	action = new ModifyFormAction();
+//}else if(cmd.equals("modifyOk")) {
+//	action = new ModifyAction();
+//}else if(cmd.equals("delete")) {
+//	action = new DeleteAction();
+//}
+//
+//
+//url = action.execute(req, resp);
+//RequestDispatcher rd = req.getRequestDispatcher(url);
+//rd.forward(req, resp);
--- src/main/java/kr/co/moran/web/dao/MemberDAO.java
+++ src/main/java/kr/co/moran/web/dao/MemberDAO.java
@@ -2,10 +2,11 @@ package kr.co.moran.web.dao;
 
 import java.io.IOException;
 import java.io.Reader;
-
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+
 
 public class MemberDAO {
 	public SqlSessionFactory factory;
