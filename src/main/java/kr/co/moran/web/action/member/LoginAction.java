a6676293bcab8bc444fd7ed85c0b74814176a659
KAH <kahback99@gmail.com>
2024-05-20 17:47:16 +0900

    [feat] LoginAction 기능 추가

--- src/main/java/kr/co/moran/web/action/member/LoginAction.java
+++ src/main/java/kr/co/moran/web/action/member/LoginAction.java
@@ -2,14 +2,26 @@ package kr.co.moran.web.action.member;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jakarta.websocket.Session;
 import kr.co.moran.web.action.Action;
+import kr.co.moran.web.dao.MemberDAO;
+import kr.co.moran.web.vo.member.MemberVO;
 
 public class LoginAction implements Action {
 
 	@Override
 	public String execute(HttpServletRequest req, HttpServletResponse resp) {
-		// TODO Auto-generated method stub
-		return null;
+		String email = req.getParameter("email");
+		String password = req.getParameter("password");
+		MemberDAO dao = new MemberDAO();
+		MemberVO vo=dao.selectMemberByEmailAndPassword(email, password);
+		System.out.println(vo);
+		if(vo!=null) {
+			HttpSession session = req.getSession();
+			session.setAttribute("memberVO", vo);
+			return "home.jsp";
+		}
+		return "loginForm.jsp"; //회원 로그인 실패
 	}
-
 }
--- src/main/webapp/home.jsp
+++ src/main/webapp/home.jsp
@@ -1,12 +1,20 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <!DOCTYPE html>
 <html>
 <head>
+
 <meta charset="UTF-8">
 <title>HOME</title>
 </head>
 <body>
 <h2>홈화면</h2>
+<c:if test="${not empty sessionScope.memberVO}">
+    <p>회원 이름, ${sessionScope.memberVO.name}!</p>
+    <p>회원 Email: ${sessionScope.memberVO.email}</p>
+    <!-- 기타 회원 정보 출력 -->
+</c:if>
 </body>
 </html>
