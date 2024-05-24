b37a643cf9e98817eae43afac89656b2a893495a
HJ <hyun1996off@gmail.com>
2024-05-24 17:50:28 +0900

    [feat] 상품주문

--- src/main/java/kr/co/moran/web/action/order/OrderDetailAction.java
+++ src/main/java/kr/co/moran/web/action/order/OrderDetailAction.java
@@ -9,6 +9,9 @@ public class OrderDetailAction implements Action {
 	@Override
 	public String execute(HttpServletRequest req, HttpServletResponse resp) {
 		
+		String pId = req.getParameter("pd_id");
+		
+		
 		return "/jsp/order/orderTest.jsp";
 	}
 
