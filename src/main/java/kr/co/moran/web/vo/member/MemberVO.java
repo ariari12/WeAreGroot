914bd0fb15b9a8aa89325b3906bae42d09fedd09
일성 <dlftjd9461@naver.com>
2024-05-30 19:05:02 +0900

    [merge] dev에 is merge하고 충돌난 부분 수정완

--- src/main/java/kr/co/moran/web/vo/member/MemberVO.java
+++ src/main/java/kr/co/moran/web/vo/member/MemberVO.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package kr.co.moran.web.vo.member;
 
 
@@ -30,37 +30,5 @@ public class MemberVO {
 	
 	
 	//변수명 정보상태, 삭제한 날짜, 관리자 유무 db 칼럼 이름 변경, Date 타입 정하기
-=======
-package kr.co.moran.web.vo.member;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class MemberVO {
-	private int mId; //회원 고유 번호
-	private String email; //회원 이메일	
-	private String pw; // 회원 비밀번호
-	private String name; // 회원 이름
-	private String profileImg; // 회원 프로필 이미지
-	private String nick; // 회원 닉네임
-	private String phone; // 회원 전화번호
-	private String regdate; // 회원 가입일
-	private int logtype; // 로그인 타입
-	private int admintype; // 관리자 유무
-	private int isMaintain;// 정보 상태
-	private String retentionPeriod; // 삭제한 회원 정보 보관 기간
-	private String birth; // 회원 생일
-	private String gender; // 성별
-	
-	private AddressVO addressVO; //주소정보
-	private MemberExitVO memberExitVO; //탈퇴 이유
-
-	
-	//변수명 정보상태, 삭제한 날짜, 관리자 유무 db 칼럼 이름 변경, Date 타입 정하기
->>>>>>> refs/heads/IS
 }
